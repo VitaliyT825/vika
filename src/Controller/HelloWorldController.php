398d81a5f6e9dc09b12cc0aa8dabbeead7ca38bc
vtarasov <vtarasov@marfa-tech.com>
2023-05-26 17:23:44 +0300

    OI update

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Swagger\Annotations as SWG;
+use MarfaTech\Bundle\ApiPlatformBundle\HttpFoundation\ApiResponse;
+use OpenApi\Annotations as OA;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @SWG\Tag(name="Hello")
+ * @OA\Tag(name="Banner")
+ *
+ * @Route(name="backofficeapi_info", path="/banner", methods={"POST"})
+ *
+ * @OA\Response(
+ *     response=ApiResponse::HTTP_CREATED,
+ *     description="created",
+ * )
  */
 class HelloWorldController
 {
-
+    public function __invoke(): ApiResponse
+    {
+        return new ApiResponse();
+    }
 }
